8f8a1e8d82141b2b8fff2beb7575f854a0c913de
KatsuyaEndoh <k.endo@s-arcana.co.jp>
2019-02-14 19:51:53 +0900

    init

---
+++ src/enkatsu/PhpOscParser/Parser.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Enkatsu\PhpOscParser;
+
+class Parser {
+}
---
+++ tests/Enkatsu/PhpOscParser/ParseTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Enkatsu\PhpOscParser\Tests;
+
+class ParseTest extends \PHPUnit_Framework_TestCase
+{
+  public function testInstance()
+  {
+      $this->assertNull(null);
+  }
+}
