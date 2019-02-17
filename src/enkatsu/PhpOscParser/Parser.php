a117bb00eaabfc158b8b7dc514e8350c5ba25b53
enkatsu <endkty0509@gmail.com>
2019-02-17 22:15:53 +0900

    :tada: runable

--- src/enkatsu/PhpOscParser/Parser.php
+++ src/enkatsu/PhpOscParser/Parser.php
@@ -29,52 +29,64 @@ class Parser
     public function parse(Collection $buf, int &$pos, int $endPos, int $timestamp=1)
     {
         $first = Reader::parseString($buf, $pos);
-        if ($first == Identifier::$BUNDLE)
+        if ($first == Identifier::$BUNDLE) {
             $this->parseBundle($buf, $pos, $endPos);
-        else
-            $this->messages->push(new Message($first, $timestamp, $this->parseData($buf, $pos)));
-        if ($pos != $endPos)
+        } else {
+            $message = new Message($first, $timestamp, $this->parseData($buf, $pos));
+            $this->messages->push($message);
+        }
+        if ($pos != $endPos) {
             error_log("The parsed data size is inconsitent with the given size: ${pos} / ${endPos}".PHP_EOL);
+        }
     }
 
     private function parseBundle(Collection $buf, int &$pos, int $endPos)
     {
         $time = Reader::parseTimetag($buf, $pos);
+        var_dump('$time');
+        var_dump($time);
         while ($pos < $endPos)
         {
-            $contentSize = Reader::parseInt($buf, $pos);
-            if (Util::isMultipleOfFour($contentSize))
-            {
-                $this->parse($buf, $pos, $pos + $contentSize, $time);
-            }
-            else
-            {
-                error_log("Given data is invalid (bundle size (${contentSize}) is not a multiple of 4).".PHP_EOL);
-                $pos += $contentSize;
-            }
+            $contentSize = Reader::parseInt($buf, $pos) / 2;
+            $this->parse($buf, $pos, $pos + $contentSize, $time);
+            // if (Util::isMultipleOfFour($contentSize))
+            // {
+            //     $this->parse($buf, $pos, $pos + $contentSize, $time);
+            // }
+            // else
+            // {
+            //     error_log("Given data is invalid (bundle size (${contentSize}) is not a multiple of 4).".PHP_EOL);
+            //     $pos += $contentSize;
+            // }
         }
     }
 
     public function parseData(Collection $buf, int &$pos): Collection
     {
-        // remove ','
-        $types = \substr(Reader::parseString($buf, $pos), 1);
-        $n = strlen($types);
-        if ($n == 0) return new Collection();
-        $data = new Collection();
-        for ($i = 0; $i < $n; ++$i)
-        {
-            switch (types[i])
+        $types = trim(Reader::parseString($buf, $pos), ',');
+        if (strlen($types) == 0) return new Collection();
+        $types = collect(str_split($types))->filter(function($type) {
+            return in_array($type, [Identifier::$INT, Identifier::$FLOAT, Identifier::$STRING, Identifier::$BLOB]);
+        });
+        return $types->map(function($type) use ($buf, &$pos){
+            switch ($type)
             {
-                case Identifier::$INT    : $data->push(Reader::parseInt($buf, $pos));    break;
-                case Identifier::$FLOAT  : $data->push(Reader::parseFloat($buf, $pos));  break;
-                case Identifier::$STRING : $data->push(Reader::parseString($buf, $pos)); break;
-                case Identifier::$BLOB   : $data->push(Reader::parseBlob($buf, $pos));   break;
+                case Identifier::$INT:
+                    return Reader::parseInt($buf, $pos);
+                    break;
+                case Identifier::$FLOAT:
+                    return Reader::parseFloat($buf, $pos);
+                    break;
+                case Identifier::$STRING:
+                    return Reader::parseString($buf, $pos);
+                    break;
+                case Identifier::$BLOB:
+                    return Reader::parseBlob($buf, $pos);
+                    break;
                 default:
-                    // Add more types here if you want to handle them.
+                    return null;
                     break;
             }
-        }
-        return $data;
+        });
     }
 }
--- src/enkatsu/PhpOscParser/Reader.php
+++ src/enkatsu/PhpOscParser/Reader.php
@@ -10,34 +10,28 @@ class Reader
 {
     public static function parseString(Collection $buf, int &$pos): string
     {
-        $size = 0;
-        $bufSize = $buf->count();
-        for (; hexdec($buf->get($pos + $size)) != 0; ++$size);
-        $val = hex2bin($buf->slice($pos, $size)->flatten()->implode(''));
-        $pos += $size;
-        return $val;
+        $lastIndex = $buf->slice($pos)->search(function($data) {
+            $bytes = collect(str_split($data, 2));
+            return $bytes->last() == '00';
+        });
+        $len = $lastIndex - $pos + 1;
+        $val = \pack('H*', $buf->slice($pos, $len)->implode(''));
+        $pos += $len;
+        return \trim($val);
     }
 
     public static function parseInt(Collection $buf, int &$pos): int
     {
-        $val = \hexdec($buf->slice($pos, 4));
-        $pos += 4;
+        $val = \hexdec($buf->get($pos));
+        $pos += 1;
         return $val;
-    }
-
-    public static function parseTimetag(Collection $buf, int &$pos): int
-    {
-        // TODO:
-        $val = \hexdec($buf->slice($pos, 8));
-        $pos += 8;
-        return $value;
     }
 
     public static function ParseFloat(Collection $buf, int &$pos)
     {
-        $str = $buf->slice($pos, 4)->flatten()->implode('');
+        $str = $buf->get($pos);
         $value = unpack("f", pack("h*", strrev($str)));
-        $pos += 4;
+        $pos += 1;
         return $value;
     }
 
@@ -49,4 +43,11 @@ class Reader
     //     pos += Util.GetBufferAlignedSize(size);
     //     return value;
     // }
+
+    public static function parseTimetag(Collection $buf, int &$pos): int
+    {
+        $val = \hexdec($buf->slice($pos, 2)->flatten()->implode(''));
+        $pos += 2;
+        return $val;
+    }
 }
